7c0b879d22191c606072d32e7a662708de4b2c57
Vlad Truta <vlad.truta@tapptitude.com>
2019-04-11 18:06:10 +0300

    Wiped everything

--- src/main/kotlin/chapter2/PureFuncExmpl.kt
+++
@@ -1,9 +0,0 @@
-package chapter2
-
-fun square(n: Int): Int = n * n // (1)
-
-fun main(args: Array<String>) {
-    println("named pure func chapter2.square = ${square(3)}")
-    val qube = { n: Int -> n * n * n } // (2)
-    println("lambda pure func qube = ${qube(3)}")
-}
